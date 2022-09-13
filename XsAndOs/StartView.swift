9b95da19b006e7a05445c90c03f94a415433bb8f
Stewart Lynch <slynch@createchsol.com>
2022-09-13 13:56:48 -0700

    Refactor Complete

--- XsAndOs/StartView.swift
+++ XsAndOs/StartView.swift
@@ -12,10 +12,15 @@ import SwiftUI
 struct StartView: View {
     @EnvironmentObject var game: GameService
     @State private var gameType: GameType = .undetermined
-    @State private var yourName = ""
+    @AppStorage("yourName") var yourName = ""
     @State private var opponentName = ""
     @FocusState private var focus: Bool
     @State private var startGame = false
+    @State private var changeName = false
+    @State private var newName = ""
+    init(yourName: String) {
+        self.yourName = yourName
+    }
     var body: some View {
             VStack {
                 Picker("Select Game", selection: $gameType) {
@@ -31,12 +36,9 @@ struct StartView: View {
                 VStack {
                     switch gameType {
                     case .single:
-                        VStack {
-                            TextField("Your Name", text: $yourName)
-                            TextField("Opponent Name", text: $opponentName)
-                        }
+                        TextField("Opponent Name", text: $opponentName)
                     case .bot:
-                        TextField("Your Name", text: $yourName)
+                        EmptyView()
                     case .peer:
                         EmptyView()
                     case .undetermined:
@@ -56,11 +58,14 @@ struct StartView: View {
                     .buttonStyle(.borderedProminent)
                     .disabled(
                         gameType == .undetermined ||
-                        gameType == .bot && yourName.isEmpty ||
-                        gameType == .single &&
-                        (yourName.isEmpty || opponentName.isEmpty)
+                        gameType == .single && opponentName.isEmpty
                     )
                     Image("LaunchScreen")
+                    Text("Your name is \(yourName)")
+                    Button("Change my name") {
+                        changeName.toggle()
+                    }
+                    .buttonStyle(.bordered)
                 }
                 Spacer()
             }
@@ -69,13 +74,23 @@ struct StartView: View {
             .fullScreenCover(isPresented: $startGame) {
                 GameView()
             }
+            .alert("Change Name", isPresented: $changeName, actions: {
+                TextField("New name", text: $newName)
+                Button("OK", role: .destructive) {
+                    yourName = newName
+                    exit(-1)
+                }
+                Button("Cancel", role: .cancel) {}
+            }, message: {
+                Text("Tapping on the OK button will quit the application so you can relaunch to use your changed name.")
+            })
             .inNavigationStack()
     }
 }
 
 struct StartView_Previews: PreviewProvider {
     static var previews: some View {
-        StartView()
+        StartView(yourName: "Sample")
             .environmentObject(GameService())
     }
 }
---
+++ XsAndOs/YourNameView.swift
@@ -0,0 +1,38 @@
+//
+// Created for XsAndOs
+// by Stewart Lynch on 2022-09-13
+// Using Swift 5.0
+//
+// Follow me on Twitter: @StewartLynch
+// Subscribe on YouTube: https://youTube.com/StewartLynch
+//
+
+import SwiftUI
+
+struct YourNameView: View {
+    @AppStorage("yourName") var yourName = ""
+    @State private var userName = ""
+    var body: some View {
+        VStack {
+            Text("This is the name that will be associated with this device.")
+            TextField("Your Name", text: $userName)
+                .textFieldStyle(.roundedBorder)
+            Button("Set") {
+                yourName = userName
+            }
+            .buttonStyle(.borderedProminent)
+            .disabled(userName.isEmpty)
+            Image("LaunchScreen")
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Xs and Os")
+        .inNavigationStack()
+    }
+}
+
+struct YourNameView_Previews: PreviewProvider {
+    static var previews: some View {
+        YourNameView()
+    }
+}
